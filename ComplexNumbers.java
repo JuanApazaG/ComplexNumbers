d843b291c34d627a76bbc4bdac3aced4caf8f652
Juan Agustin Apaza <111205513+JuanApazaG@users.noreply.github.com>
2024-03-07 14:31:23 -0400

    parte de agus subida a la ramaAgustin

--- ComplexNumbers.java
+++ ComplexNumbers.java
@@ -1,38 +1,45 @@
-/**
-*Lab 2 Ingenieri de software
-* Operaciones sobre numeros complejos
-*@author Adalid Flores Gomez
-*@version 1.0
-*@date 2024-02-29
-*/
 class HelloWorld {
     public static void main(String[] args) {
-        Complex x=new Complex(2,3);
-        Complex y=new Complex(-4,3);
-        Complex z=new Complex(0,0);
-        z=x.adition(y);
-        System.out.println("Adition: " + z.toStr());
+        Complex x = new Complex(2, 3);
+        Complex y = new Complex(-4, 3);
+        Complex z = new Complex(0, 0);
+        
+        z = x.addition(y);
+        System.out.println("Addition: " + z.toString());
+        
         z = x.subtraction(y);
-        System.out.println("Subtraction: " + z.toStr());
+        System.out.println("Subtraction: " + z.toString());
+        
         z = x.multiplication(y);
-        System.out.println("Multiplication: " + z.toStr());
+        System.out.println("Multiplication: " + z.toString());
+        
+        z = x.division(y);
+        System.out.println("Division: " + z.toString());
+        
+        Complex conjugateX = x.conjugate();
+        Complex conjugateY = y.conjugate();
+        
+        System.out.println("Conjugate of x: " + conjugateX.toString());
+        System.out.println("Conjugate of y: " + conjugateY.toString());
     }
 }
 
-class Complex{
-    int real;
-    int img;
-    Complex(int r,int i){
-        this.real=r;
-        this.img=i;
+class Complex {
+    double real;
+    double img;
+
+    Complex(double r, double i) {
+        this.real = r;
+        this.img = i;
     }
-    Complex adition(Complex y){
-        Complex z=new Complex(0,0);
-        z.real=this.real - y.real;
-        z.img=this.img - y.img;
-        return (z);
+
+    Complex addition(Complex y) {
+        Complex z = new Complex(0, 0);
+        z.real = this.real + y.real;
+        z.img = this.img + y.img;
+        return z;
     }
-    
+
     Complex subtraction(Complex y) {
         Complex z = new Complex(0, 0);
         z.real = this.real - y.real;
@@ -47,8 +54,19 @@ class Complex{
         return z;
     }
 
-    String toStr(){
-        return (String.valueOf(this.real)) + "+" + String.valueOf(this.img);
-        
+    Complex division(Complex y) {
+        Complex z = new Complex(0, 0);
+        double denominator = y.real * y.real + y.img * y.img;
+        z.real = (this.real * y.real + this.img * y.img) / denominator;
+        z.img = (this.img * y.real - this.real * y.img) / denominator;
+        return z;
+    }
+
+    Complex conjugate() {
+        return new Complex(this.real, -this.img);
+    }
+
+    public String toString() {
+        return "(" + this.real + " + " + this.img + "i)";
     }
 }
